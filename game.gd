fb7334eebb7ca5efd8f0453d2577561e41f5b245
Tiago Nunes <tiago.nunes.lx@gmail.com>
2025-02-24 17:28:04 +0000

    Added Start sound

--- game.gd
+++ game.gd
@@ -5,7 +5,7 @@ var score2 := 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	$Music.play()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -28,7 +28,7 @@ func start_game():
 	$Player2.start()
 	$Player2.isAi = true
 	$Ball.start()
-	$Music.play()
+	$StartSound.play()
 
 func update_score():
 	$Score1.text = str(score1)
